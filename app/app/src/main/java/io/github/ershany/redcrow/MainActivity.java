8cb955f4488313e4603fbcfd0f99ef6b3baa6640
Ershany <bradyjessup@hotmail.com>
2017-09-21 11:45:30 -0400

    Added basic sending and receiving and reading SMS and added their user permissions. Also added square and round icons with their corresponding mipmaps.

--- app/app/src/main/java/io/github/ershany/redcrow/MainActivity.java
+++ app/app/src/main/java/io/github/ershany/redcrow/MainActivity.java
@@ -1,13 +1,35 @@
 package io.github.ershany.redcrow;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmsManager smsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        smsManager = SmsManager.getDefault();
     }
+
+    public void searchButtonOnClick(View v) {
+        EditText searchText = (EditText) findViewById(R.id.searchEditText);
+
+        // Later will want to change the last two parameters to a value so we can tell if the sms was sent and received. Thus we can update the frontend
+        smsManager.sendTextMessage("6136330139", null, searchText.getText().toString(), null, null);
+
+        searchText.setText("");
+
+        // Hide the keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
 }
---
+++ app/app/src/main/java/io/github/ershany/redcrow/SmsListener.java
@@ -0,0 +1,59 @@
+package io.github.ershany.redcrow;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Brady on 9/21/2017.
+ */
+
+public class SmsListener extends BroadcastReceiver {
+
+    private final SmsManager smsManager = SmsManager.getDefault();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+
+        if(bundle != null) {
+            try {
+                SmsMessage[] smsMessages;
+
+                // Try to avoid using the deprecated createFromPdu but if its an old version, we will have to use it
+                if(Build.VERSION.SDK_INT >= 19) { // KITKAT
+                    smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                } else {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+
+                    smsMessages = new SmsMessage[pdus.length];
+                    for(int i = 0; i < pdus.length; ++i) {
+                        smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    }
+                }
+
+                // Loop through all of the received SMS and filter them to only read the ones from the server
+                for(int i = 0; i < smsMessages.length; ++i) {
+                    String phoneNumber = smsMessages[i].getDisplayOriginatingAddress();
+                    String message = smsMessages[i].getDisplayMessageBody();
+
+                    Toast toast = Toast.makeText(context, "Sender Number: " + phoneNumber +", Message: " + message, Toast.LENGTH_LONG);
+                    toast.show();
+
+                    Log.i("SMS Number", phoneNumber);
+                    Log.i("SMS Message", message);
+                }
+            } catch(Exception e) {
+                Log.d("Exception - SmsListener", e.getMessage());
+            }
+        }
+    }
+}
