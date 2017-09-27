224ad0f0678543bb7d8160e3c443e4688b6171aa
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-09-27 16:55:27 -0400

    added winston logger

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
+'use strict'
+
 const mysql = require('mysql')
 const express = require('express')
+const winston = require('winston') // make a seperate logger file?
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 
+const PORT = 9001
 const app = express()
+
+const ERROR_CODES = {
+	CHECK_BYTE: 1,
+	DATA: 2
+}
 
 var con = mysql.createConnection({
 	host: 'localhost',
@@ -10,26 +19,61 @@ var con = mysql.createConnection({
 	password: 'potato'
 })
 
+// google search api key
+// AIzaSyD62XwkP-5dwJ6eaewoySIqfYvjctvAGKs
+
 con.connect((err) => {
 	if(err)
 		throw err
-	console.log('connected')
+	winston.info('Connected to MySQL Database')
 })
 
 // Change to a POST request?
 app.get('/sms', (req, res) => {
-	// Add colours to logger
-	// 17:36:08 - Received SMS: Heyo
-	console.log(new Date().toLocaleTimeString(), '- Received SMS:', req.query.Body)
-	console.log(req.query)
+	winston.info(new Date().toLocaleTimeString(), '- Received SMS:', req.query.Body)
+	winston.info(req.query)
+
+	if(!(req && req.query && req.query.Body)) {
+		winston.warn('No Query Received')
+		return res.sendStatus(500)
+	}
+
+	var decoded = decode(req.query.Body)
+
+	if(decoded.length < 4) {
+		winston.warn('Received Insufficient Data')
+		return res.sendStatus(500)
+	}
+
+	if(!decoded.startsWith('E')) {
+		winston.warn('Check Byte Fail')
+		return res.sendStatus(500)
+	}
+
+	let app_id = decoded.charAt(1) // charCodeAt
+	let msg_id = decoded.charCodeAt(2)
+	let msg_body = decoded.substr(3)
+	let reply = 'Unknown Command'
+	switch(app_id) {
+		case '0': // 0
+			winston.info('Google Search Request: %s', msg_body)
+			reply = 'cats are great, ' + msg_body
+			break;
+		default:
+			winston.warm('Unimplemented Application')
+			break;
+	}
 
 	const twiml = new MessagingResponse()
-	twiml.message('Unknown Command.')
+	twiml.message(encode(reply))
 
 	res.writeHead(200, {'Content-Type': 'text/xml'})
 	res.end(twiml.toString())
 })
 
-app.listen(9001, () => {
-	console.log('Express @ localhost:9001')
+function decode(str) { return str }
+function encode(str) { return str }
+
+app.listen(PORT, () => {
+	winston.info('Express @ localhost:%d', PORT)
 })
