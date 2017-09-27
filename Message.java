55e6cda7643211b5fcf06ca327e7d7aaa3bd8f2c
Alail01 <tim.j.noah@gmail.com>
2017-09-27 16:51:26 -0400

    Add files via upload

--- Message.java
+++ Message.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class Message extends encryption{
-
+  
   public static Scanner sc = new Scanner(System.in);
 
-  public static void main(String[] args){
-	char[] c = new char[100];
-	for (int i = 0; i < 100; i++) {
-		c[i] = 'a';
-	}
-    int k = 10;
-    System.out.println(c);
-    
-    encrypt(c,k);
-     System.out.println(c);
+  public Message(String s,int k) {
+	super(s,k);
+  }
+
+
+  public static void main(String[] arg) {
+	  String s = "TEST 123";
+	  int k = 2;
+	  Message m = new Message(s,k);
+	  System.out.println(m.getMessage());
+	  m.toEncrypt();
+	  System.out.println(m.getMessage());
+	  m.toDecrypt();
+	  System.out.println(m.getMessage());
   }
 }
--- encryption.java
+++ encryption.java
@@ -2,121 +2,131 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class encryption {
+public class encryption{
 
-  private ArrayList<Character> message;
-  private int key;//??????
+	private ArrayList<Character> message = new ArrayList<Character>();
+	private int key;//??????
 
-  public encryption(ArrayList<Character> x,int k){
-    message = x;
-    key = k;
-
-  }
-
-
-  public void toEncrypt(){
-    for (int a = 0;a != this.message.size();a ++) {
-      int change = this.message.get(a);
-      while (this.key > 94) {
-    	  this.key -= 94;
-      }
-      if (change >= 32 || change <= 126) {
-        if (change+this.key > 126) {
-          change = change+this.key-94;
-        }
-        else if (change+this.key < 32) {
-          change = change+this.key+94;
-        }
-        else {
-          change = change+this.key;
-        }
-
-        this.message.set(a, (char)change);
-
-
-      }
-    }
-    
-    this.split();
-    
-  }
-  
-  private void split() {
-	  int a,b;
-	  
-	  for (int i = 0; i < (this.message.size()/8);i++){
-		  a = ((i+1)*8)-8;
-		  b = ((i+1)*8);
-		  List<Character> tmp =  (this.message).subList(a,b);
-		  //System.out.println((this.message).subList(a, b));
-		  flips(tmp);
-		  //System.out.println(this.message);
-
-	  }
-	  
-	  //System.out.println(b);
-	  if ((this.message.size()%8) != 0) {
-		  b = this.message.size()-((this.message.size()%8));
-		  List<Character> tmp =  (this.message).subList(b,this.message.size());
-		  //System.out.println((this.message).subList(b, this.message.size()));
-		  flips(tmp);
-		  //System.out.println(this.message);
-		  //System.out.println((this.message).subList(b, this.message.size()));
-	  }
-  }
-  
-  private static void flips(List<Character> tmp) {
-	  //char tmp;
-	  Collections.reverse(tmp);
-	  /*for (int i = 0;i<(c.size()/2);i++) {
-		  System.out.println(c.get(i));
-		  tmp = c.get(c.size()-1-i);
-		  c.set(c.size()-1-i, c.get(i));
-		  c.set(i, tmp);
-		 System.out.println(c.get(i));
-	  }*/
-  }
-
-  public static void main(String[] args){
-	ArrayList<Character>z = new ArrayList<Character>();
-	for (int i = 0; i < 8; i++){
-	  z.add((char) (97+i));
+	@Override
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < this.message.size();i++) {
+			s += this.message.get(i);
+		}
+		return s;	  
 	}
-	System.out.println(z);
-    flips(z);
-	System.out.println(z);
 	
-    ArrayList<Character> c = new ArrayList<Character>();
-    c.add('T');
-    c.add('h');
-    c.add('I');
-    c.add('s');
-    c.add(' ');
-    c.add('i');
-    c.add('S');
-    c.add(' ');
-    c.add('A');
-    c.add(' ');
-    c.add('T');
-    c.add('e');
-    c.add('s');
-    c.add('t');
-    c.add(' ');
-    c.add('Z');
-    c.add('Y');
-    c.add('x');
-    c.add('v');
-    c.add('1');
-    c.add('2');
-    c.add('3');
-    c.add('4');
-    encryption s = new encryption(c,899);
-    
-    System.out.println(s.message);
-    s.toEncrypt();
-    System.out.println(s.message);
+	public encryption(String s, int k){
+		for (int i = 0; i < s.length() ; i++) {
+			//System.out.println(s.toCharArray()[i]);
+			message.add(s.toCharArray()[i]);
+		}
+		while (k > 94) {
+			k-=94;
+		}
+		key = k;
+	
+	}
+	
+	public String getMessage() {
+		 return this.toString();
+	 }
+	
+	protected void toDecrypt() {
+		for (int i = 0; i != this.message.size(); i++) {
+			int change = this.message.get(i);
+			if (change -this.key < 32) {
+				change = change-this.key+94;
+			}
+			else if (change+this.key > 126) {
+				change = change+this.key - 94;
+			}
+			else {
+				change = change - this.key;
+			}
+			this.message.set(i, (char)change);	
+		}
+		this.split();
+	}
+	
+	protected void toEncrypt(){
+		for (int a = 0;a != this.message.size();a ++) {
+			int change = this.message.get(a);
+			if (change >= 32 || change <= 126) {
+				if (change+this.key > 126) {
+					change = change+this.key-94;
+				}
+				else if (change+this.key < 32) {
+					change = change+this.key+94;
+				}
+				else {
+					change = change+this.key;
+				}
+	
+				this.message.set(a, (char)change);
+	
+	
+			}
+		}
+	    
+	    this.split();
+	    
+	  }
+	  
+	  private void split() {
+		  int a,b;
+		  
+		  for (int i = 0; i < (this.message.size()/8);i++){
+			  a = ((i+1)*8)-8;
+			  b = ((i+1)*8);
+			  List<Character> tmp =  (this.message).subList(a,b);
+			  //System.out.println((this.message).subList(a, b));
+			  flips(tmp);
+			  //System.out.println(this.message);
+	
+		  }
+		  
+		  //System.out.println(b);
+		  if ((this.message.size()%8) != 0) {
+			  b = this.message.size()-((this.message.size()%8));
+			  List<Character> tmp =  (this.message).subList(b,this.message.size());
+			  //System.out.println((this.message).subList(b, this.message.size()));
+			  flips(tmp);
+			  //System.out.println(this.message);
+			  //System.out.println((this.message).subList(b, this.message.size()));
+		  }
+	  }
+	  
+	  private static void flips(List<Character> tmp) {
+		  //char tmp;
+		  Collections.reverse(tmp);
+		  /*for (int i = 0;i<(c.size()/2);i++) {
+			  System.out.println(c.get(i));
+			  tmp = c.get(c.size()-1-i);
+			  c.set(c.size()-1-i, c.get(i));
+			  c.set(i, tmp);
+			 System.out.println(c.get(i));
+		  }*/
+	  }
 
-    
-  }
-
-}
+	  
+	  public static void main(String[] args){
+		ArrayList<Character>z = new ArrayList<Character>();
+		for (int i = 0; i < 8; i++){
+		  z.add((char) (97+i));
+		}
+		System.out.println(z);
+	    flips(z);
+		System.out.println(z);
+		
+		String c = "THIS IS A TEST";
+	    encryption s = new encryption(c,000);
+	    
+	    System.out.println(s.message);
+	    s.toEncrypt();
+	    System.out.println(s.message);
+	
+	    
+	  }
+	
+	}
