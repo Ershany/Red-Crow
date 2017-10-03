a31bd82c80e8c01506705969010bef2adcc9c61c
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-10-03 18:03:01 -0400

    moudlarized server code and devlog 4

--- server/app.js
+++ server/app.js
@@ -1,68 +1,24 @@
 'use strict'
 
-const compression = require('compression')
-const orm = require('orm')
-const MigrateTask = require('migrate-orm2')
 const express = require('express')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
+const compression = require('compression')
 const log = require('./log')
+const dbms = require('./dbms')
 
 const PORT = 9001
 const app = express()
 
-const ERROR_CODES = {
-	CHECK_BYTE: 1,
-	DATA: 2
-}
+const ERRORS = [
+	{ code: 1 << 0, message: 'Check Byte Fail' },
+	{ code: 1 << 1, message: 'No Query Received' },
+	{ code: 1 << 2, message: 'Received Insufficient Data' },
+	{ code: 1 << 3, message: 'Unimplemented Application' }
+]
 
 app.use(compression())
 
-// google search api key: AIzaSyD62XwkP-5dwJ6eaewoySIqfYvjctvAGKs
-
-// TODO: Add foreign key relationships
-orm.connect('mysql://root:potato@localhost/smsblitz', (err, db) => {
-	if(err) throw err
-	log.info('Connected to MySQL Database')
-	var task = new MigrateTask(db.driver)
-	log.info("Migration framework initialized for Node ORM")
-	
-	// Define tables
-	var User = db.define('User', {
-		id: { type: 'serial', key: true },
-		phonenumber: { type: 'text', size: 20, required: true, index: true }, // unique: true
-		blacklisted: { type: 'boolean', defaultValue: false }
-	})
-	
-	var Message = db.define('Message', {
-		id: { type: 'serial', key: true },
-		timestamp: { type: 'date', time: true, required: true, defaultValue: new Date() }
-	})
-	
-	var ServerSearchMessage = db.define('ServerSearchMessage', {
-		id: { type: 'serial', key: true },
-		response: { type: 'text', required: true }
-	})
-	ServerSearchMessage.hasOne('message', Message, {required: true})
-	
-	var ClientSearchMessage = db.define('ClientSearchMessage', {
-		id: { type: 'serial', key: true },
-		query: { type: 'text', required: true }
-	})
-	ClientSearchMessage.hasOne('message', Message, {required: true})
-	
-	// Add tables to the db
-	db.sync((err) => {
-		if(err) throw err
-		
-		// Add row just for testing
-		// User.create({ phonenumber: '613-633-0136'}, (err) => {
-		// 	if(err) throw err
-		// })
-		// Message.create({}, (err) => {
-		// 	if(err) throw err
-		// })
-	})
-})
+dbms.connect('mysql://root:potato@localhost/smsblitz')
 
 app.get('/sms', (req, res) => {
 	log.info('Received SMS:', req.query.Body)
@@ -78,7 +34,7 @@ app.get('/sms', (req, res) => {
 
 	var decoded = decode(req.query.Body)
 	let app_id = decoded.charAt(1) // charCodeAt
-	let msg_id = decoded.charCodeAt(2)
+	let msg_id = decoded.charAt(2) // charCodeAt
 	let msg_body = decoded.substr(3)
 
 	if(decoded.length < 4) {
@@ -127,7 +83,7 @@ app.get('/sms', (req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/xml'})
 	res.end(twiml.toString())
 
-	lon.info('Sent %d bytes to user %d', text_reply.length, 6137954472) // TODO: get actual number
+	log.info('Sent %d bytes to user %d', text_reply.length, 6137954472) // TODO: get actual number
 })
 
 function decode(str) { return str }
