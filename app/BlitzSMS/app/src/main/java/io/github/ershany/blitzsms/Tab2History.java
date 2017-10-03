c95c5c47aa4e28985f8de0042218dfb41d593c83
Ershany <bradyjessup@hotmail.com>
2017-10-03 09:40:29 -0400

    rebuilt the frontend application. Also built the UI from the groundup with new features and nicer layouts.

---
+++ app/BlitzSMS/app/src/main/java/io/github/ershany/blitzsms/Tab2History.java
@@ -0,0 +1,21 @@
+package io.github.ershany.blitzsms;
+
+/**
+ * Created by Brady on 10/2/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2History extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2history, container, false);
+        return rootView;
+    }
+}
---
+++ app/BlitzSMS/app/src/main/java/io/github/ershany/blitzsms/Tab3Settings.java
@@ -0,0 +1,21 @@
+package io.github.ershany.blitzsms;
+
+/**
+ * Created by Brady on 10/2/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Settings extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3settings, container, false);
+        return rootView;
+    }
+}
