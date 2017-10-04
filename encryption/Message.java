fcfb6711dccdfbbb559724a93e69d8a13ec100b2
Alail01 <tim.j.noah@gmail.com>
2017-10-04 16:38:19 -0400

    Finished This Weeks Taskes
    Message.java is a testing class, where everything is just a main, to test Encryption

--- encryption/Message.java
+++ encryption/Message.java
@@ -1,6 +1,7 @@
+
 import java.util.Scanner;
-//Just a basic example of how the messages should be used with encryption
-public class Message extends encryption{
+
+public class Message extends Encryption{
   
   public static Scanner sc = new Scanner(System.in);
 
@@ -10,7 +11,12 @@ public class Message extends encryption{
 
 
   public static void main(String[] arg) {
-	  String s = "TEST 123";
+	  String s = "012";
+	  for (int i = 0; i < 100; i++) {
+		  s+="This is a test"; // This will change to illegable wording when encoded
+	  }
+	  System.out.println(s);
+	  long start = System.nanoTime();
 	  int k = 2;
 	  Message m = new Message(s,k);
 	  System.out.println(m.getMessage());
@@ -18,5 +24,10 @@ public class Message extends encryption{
 	  System.out.println(m.getMessage());
 	  m.toDecrypt();
 	  System.out.println(m.getMessage());
+	  long end = System.nanoTime();
+	  long dur = (end - start);
+	  System.out.println(dur/1000000);
+	 
+	  System.out.println((m.strip())[2]);
   }
 }
--- encryption/encryption.java
+++ encryption/encryption.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class encryption{
-
+public class Encryption{
+	private byte[] gen = new byte[3];
 	private ArrayList<Character> message = new ArrayList<Character>();
-	private int key;//??????
-	
+	private int key;
+
 	/*
 	Name: toString()
 	Purpose: Needed a way to make the array back to a string
@@ -20,15 +20,19 @@ public class encryption{
 		}
 		return s;	  
 	}
+	
 	/*
 	Name: encryption(s,k)
 	Purpose: Basic Constructor
 	In/outs: s -> String -> in -> the message that is to be passed
 		 k -> int -> in -> the 'key', will be the last 3 numbers of the phone
 	*/
-	public encryption(String s, int k){
-		for (int i = 0; i < s.length() ; i++) {
-			//System.out.println(s.toCharArray()[i]);
+	public Encryption(String s, int k){
+		gen[0] = s.getBytes()[0];
+		gen[1] = s.getBytes()[1];
+		gen[2] = s.getBytes()[2];
+		
+		for (int i = 3; i < s.length() ; i++) {
 			message.add(s.toCharArray()[i]);
 		}
 		while (k > 94) {
@@ -39,20 +43,29 @@ public class encryption{
 	}
 	
 	/*
+	 Name: strip()
+	 Purpose: return generic values that at is at the start
+	 In/Out: 
+	 */
+	public byte[] strip () {
+		return gen;
+	}
+	
+	/*
 	Name: getMessage()
 	Purpose: public function to get the message from the encryption
 	In/outs: 
 	*/
 	public String getMessage() {
 		 return this.toString();
-	 }
+	}
 	
 	/*
 	Name: toDecrypt()
 	Purpose: Called on the message, to decrypt the message
 	In/outs: 
 	*/
-	protected void toDecrypt() {
+	public void toDecrypt() {
 		for (int i = 0; i != this.message.size(); i++) {
 			int change = this.message.get(i);
 			if (change -this.key < 32) {
@@ -74,7 +87,7 @@ public class encryption{
 	Purpose: called on the message to encrypt it
 	In/outs: 
 	*/
-	protected void toEncrypt(){
+	public void toEncrypt(){
 		for (int a = 0;a != this.message.size();a ++) {
 			int change = this.message.get(a);
 			if (change >= 32 || change <= 126) {
@@ -89,8 +102,7 @@ public class encryption{
 				}
 	
 				this.message.set(a, (char)change);
-	
-	
+				
 			}
 		}
 	    
@@ -103,65 +115,32 @@ public class encryption{
 	Purpose: will split the array into smaller arrays (8 chars) so it can be fliped
 	In/outs: 
 	*/
-	  private void split() {
+	private void split() {
 		  int a,b;
 		  
 		  for (int i = 0; i < (this.message.size()/8);i++){
 			  a = ((i+1)*8)-8;
 			  b = ((i+1)*8);
 			  List<Character> tmp =  (this.message).subList(a,b);
-			  //System.out.println((this.message).subList(a, b));
 			  flips(tmp);
-			  //System.out.println(this.message);
 	
 		  }
-		  
-		  //System.out.println(b);
+
 		  if ((this.message.size()%8) != 0) {
 			  b = this.message.size()-((this.message.size()%8));
 			  List<Character> tmp =  (this.message).subList(b,this.message.size());
-			  //System.out.println((this.message).subList(b, this.message.size()));
 			  flips(tmp);
-			  //System.out.println(this.message);
-			  //System.out.println((this.message).subList(b, this.message.size()));
 		  }
 	  }
-	  
+	
 	/*
 	Name: flips(tmp)
 	Purpose: Will filp the tmp list
 	In/outs: tmp -> List<Char> -> in/out -> flips the list.
 	*/
 	  private static void flips(List<Character> tmp) {
-		  //char tmp;
 		  Collections.reverse(tmp);
-		  /*for (int i = 0;i<(c.size()/2);i++) {
-			  System.out.println(c.get(i));
-			  tmp = c.get(c.size()-1-i);
-			  c.set(c.size()-1-i, c.get(i));
-			  c.set(i, tmp);
-			 System.out.println(c.get(i));
-		  }*/
 	  }
 
-	  /*
-	  public static void main(String[] args){
-		ArrayList<Character>z = new ArrayList<Character>();
-		for (int i = 0; i < 8; i++){
-		  z.add((char) (97+i));
-		}
-		System.out.println(z);
-	    flips(z);
-		System.out.println(z);
-		
-		String c = "THIS IS A TEST";
-	    encryption s = new encryption(c,000);
-	    
-	    System.out.println(s.message);
-	    s.toEncrypt();
-	    System.out.println(s.message);
-	
-	    
-	  }*/
 	
 	}
