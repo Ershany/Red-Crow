bb69295f35c100e14769ace8d5d953e29daf609b
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-10-16 13:12:31 -0400

    exec for encryption

--- server/async_callbacks/bears.js
+++ server/async_callbacks/bears.js
@@ -1,8 +1,13 @@
 var fs = require('fs')
 
 function replySMS(message, done) {
-	if(message.auth != 69)
-		return done(null, 'Unauthorized Token')
+	let error_code = 0
+	let reply = 'empty'
+
+	if(message.auth != 69) {
+		error_code = 1
+		reply = 'Unauthorized Token'
+	}
 
 	switch(message.app_id) {
 		case 0:
@@ -12,14 +17,14 @@ function replySMS(message, done) {
 			webpageFetch(message.body)
 			break;
 		default:
-			done('Unimplemented Application')
+			error_code = 2
+			reply = 'Unimplemented Application'
 			break;
 	}
 
-
 	function googleSearch(query) {
 		fs.readFile(query, function (err, results) {
-			if(err)
+			if(err) 
 				return done(err)
 			done(null, results)
 		})
@@ -34,11 +39,15 @@ let message = {
 	auth: 69,
 	app_id: 0,
 	msg_id: 0,
-	body: 'TheBeatles.txt'
+	body: 'async_callbacks/TheBeatles.txt'
 }
 
 replySMS(message, function(err, res) {
 	if(err)
-		return console.log(err) // error sms
-	console.log(res.toString()) // worked sms
+		console.log(err) // no sms sent
+	console.log(res.toString())
+	return res.toString()
+	// sendEncodedSMS(res.toString())
 })
+
+console.log('Reply', reply)
--- server/encryption.js
+++ server/encryption.js
@@ -1,11 +1,25 @@
 'use strict'
 
-function decode(str) {
-	return str
+let exec = require('child_process').exec
+
+const bin_folder = '' // ./bin/ do i need to specify package for this to work
+
+function encode(str, num, done) {
+	let cmd = 'java Encode ${str} ${num}'
+	exec(cmd, done)
 }
 
-function encode(str) {
-	return str
+function decode(str, num, done) {
+	let cmd = 'java Decode ${str} ${num}'
+	exec(cmd, done)
 }
+
+encode("hello", 472, (err, stdout, stderr) => {
+	console.log('error: %s\nstdout: %s\nstderr: %s', err, stdout, stderr)
+})
+
+decode("hello", 472, (err, stdout, stderr) => {
+	console.log('error: %s\nstdout: %s\nstderr: %s', err, stdout, stderr)
+})
 
 module.exports = { decode, encode }
