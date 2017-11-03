ba05269e6a777f062fbf0e11373e3a2c3e4e22b7
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-11-03 17:06:33 -0400

    ignore some gradles and small test update

--- server/test/appTest.js
+++ server/test/appTest.js
@@ -35,15 +35,20 @@ describe('App', () => {
 						throw err
 					let text = res.Response.Message[0]
 					let sms = textToSMSObj(text)
+					let bodyLines = sms.body.split(/\n/)
 					
 					assert.equal(sms.err, 0)
 					assert.equal(sms.app, 0)
 					assert.equal(sms.msg, 0)
 
-					let bodyLines = sms.body.split(/\n/)
+					// assert.typeOf(sms.err, 'number')
+					// assert.typeOf(sms.app, 'number')
+					// assert.typeOf(sms.msg, 'number')
+
 					assert.typeOf(sms.body, 'string')
 					assert.isAbove(sms.body.length, 0)
-					assert.equal(bodyLines.length, 9) // 3 queries, 3 lines each
+					assert.isBelow(sms.body.length, 2000) // 2k char limit
+					assert.lengthOf(bodyLines, 9) // 3 queries, 3 lines each
 
 					for(let line of bodyLines) {
 						assert.typeOf(line, 'string')
