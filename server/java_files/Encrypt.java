7d65d97bbd4ba4d9b3ce0cefcdbebc23fce81458
Alail01 <tim.j.noah@gmail.com>
2017-11-16 21:04:01 -0500

    encrypt fix

--- server/java_files/Encrypt.java
+++ server/java_files/Encrypt.java
@@ -1,29 +1,47 @@
-package java_files;
+//package java_files;
 
 public class Encrypt{
   public static void main(String[] arg) {
-    String message = arg[1];
+    String[] message = arg;
     int z = Integer.parseInt(arg[0]);
     String newMessage = "";
-    while (z > 126){
-      z-=94;
+    while (z > 52){
+      z-=52;
     }
-    for (int a = 0;a < message.length();a ++) {
-      int change = message.charAt(a);
-      if (change >= 32 || change <= 126) {
-        if (change+z > 126) {
-          change = change+z-94;
+    //System.out.print(z+"\n");
+    for(int j = 1;j<message.length;j++){
+    for (int a = 0;a < message[j].length();a ++) {
+      int change = message[j].charAt(a);
+        //System.out.print(change+"\n");
+        if (change <= 90&&change >= 65){
+          //System.out.print("UPPER\n");
+          if (change + z > 90){
+            change = change + z - 26;
+          }
+          else if (change + z < 65){
+            change = change + z + 26;
+          }
+          else{
+            change = change + z;
+          }
         }
-        else if (change+z < 32) {
-          change = change+z+94;
+        else if (change >= 96&&change <= 122){
+          //System.out.print("LOWER\n");
+          if (change + z > 122){
+            change = change + z - 26;
+          }
+          else if (change + z < 96){
+            change = change + z + 26;
+          }
+          else{
+            change = change + z;
+          }
         }
-        else {
-          change = change+z;
-        }
-      }
+        //System.out.println(change+"\n");
       newMessage += (char)change;
     }
-
-    System.out.print(newMessage);
+    newMessage += " ";
   }
+  System.out.print(newMessage);
+}
 }
