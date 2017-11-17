1cdda3ece74bf578095ba48a691cd27e65d65c12
Alail01 <tim.j.noah@gmail.com>
2017-11-16 19:22:02 -0500

    Encrypt DONE? again...

---
+++ server/java_files/Encrypt.java
@@ -0,0 +1,27 @@
+package java_files;
+
+public class Encrypt{
+  public static void main(String[] arg) {
+    String message = arg[1];
+    int z = Integer.parseInt(arg[0]);
+    String newMessage = "";
+
+    for (int a = 0;a < message.length();a ++) {
+      int change = message.charAt(a);
+      if (change >= 32 || change <= 126) {
+        if (change+z > 126) {
+          change = change+z-94;
+        }
+        else if (change+z < 32) {
+          change = change+z+94;
+        }
+        else {
+          change = change+z;
+        }
+      }
+      newMessage += (char)change;
+    }
+
+    System.out.println(newMessage);
+  }
+}
